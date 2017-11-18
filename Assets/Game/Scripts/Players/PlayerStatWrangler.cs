df38a0b1adb3c8d2b1acfdb7994c4b2445f750e7
Sam <gebharsj@gmatc.matc.edu>
2017-11-18 11:26:37 -0600

    added some sample upgrades, created PlayerStatWrangler

---
+++ Assets/Game/Scripts/Players/PlayerStatWrangler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStatWrangler : MonoBehaviour {
+
+	/*
+	 * This script should:
+	 *	- grab the list of selected upgrades
+	 *	- cycle through the upgrades and send the stat changes to the necessary scripts
+	 */
+
+	public List<Upgrade> activeUpgrades;
+
+	// Use this for initialization
+	void Start () {
+		foreach(Upgrade upgrade in activeUpgrades)
+		{
+			print(upgrade.name + '\n' +
+				"ability cooldown percent: " + upgrade.abilityCooldownPercent + '\n' +
+				"aim zoom value: " + upgrade.aimZoomValue + '\n' +
+				"component count: " + upgrade.componentCount + '\n' +
+				"forward speed percent: " + upgrade.forwardSpeedPercent + '\n' +
+				"life count: " + upgrade.lifeCount + '\n' +
+				"max health percent: " + upgrade.maxHealthPercent + '\n' +
+				"turning speed percent: " + upgrade.turningSpeedPercent + '\n' +
+				"weapon damage percent: " + upgrade.weaponDamagePercent);
+		}
+	}
+}
